e283fcaabf8daaeb8b9e1047b740a9619fbdbfc4
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-08-24 11:41:17 +0600

    get form data

--- src/Components/NetFeeRegForm.jsx
+++ src/Components/NetFeeRegForm.jsx
@@ -1,38 +1,168 @@
 import React from "react";
 
 const NetFeeRegForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const instituteName = e.target.instituteName.value;
+    const adminName = e.target.adminName.value;
+    const mobile = e.target.mobile.value;
+    const email = e.target.email.value;
+    const division = e.target.division.value;
+    const address = e.target.address.value;
+    const packageName = e.target.package.value;
+    const referenceName = e.target.referenceName.value;
+    const referenceMobile = e.target.referenceMobile.value;
+    const registerObject = {instituteName,adminName, mobile, email, division,address, packageName, reference: {referenceName, referenceMobile}}
+    console.log(registerObject)
+  }
   return (
     <div className="container mx-auto">
       <h2 className="text-center font-semibold text-4xl m-6">
         Registration Form
       </h2>
-      <form action="" className="max-w-xl mx-auto">
+      <form onSubmit={handleSubmit} action="" className="max-w-xl mx-auto">
         <div className="mb-5">
-          <label htmlFor="name" className="font-semibold text-lg">
+          <label htmlFor="instituteName" className="font-semibold text-lg">
             Institute Name:
           </label>{" "}
           <br />
           <input
             type="text"
-            name="name"
-            id="name"
+            name="instituteName"
+            id="instituteName"
             placeholder=""
             className="input input-bordered input-info w-full max-w-lg"
           />
         </div>
         <div className="mb-5">
-          <label htmlFor="name" className="font-semibold text-lg">
-            Institute Name:
+          <label htmlFor="adminName" className="font-semibold text-lg">
+            Admin Name:
           </label>{" "}
           <br />
           <input
             type="text"
-            name="name"
-            id="name"
+            name="adminName"
+            id="adminName"
             placeholder=""
             className="input input-bordered input-info w-full max-w-lg"
           />
         </div>
+        <div className="mb-5">
+          <label htmlFor="mobile" className="font-semibold text-lg">
+            Mobile No:
+          </label>{" "}
+          <br />
+          <input
+            type="tel"
+            name="mobile"
+            id="mobile"
+            placeholder=""
+            className="input input-bordered input-info w-full max-w-lg"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="email" className="font-semibold text-lg">
+            Email:
+          </label>{" "}
+          <br />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder=""
+            className="input input-bordered input-info w-full max-w-lg"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="division" className="font-semibold text-lg">
+            Select Division:
+          </label>{" "}
+          <br />
+          <select
+            name="division"
+            id=""
+            className="select select-info w-full max-w-lg"
+          >
+            <option value="">...</option>
+            <option value="Rajshahi">Rajshahi</option>
+            <option value="Dhaka">Dhaka</option>
+            <option value="Sylhet">Sylhet</option>
+          </select>
+        </div>
+        <div className="mb-5">
+          <label htmlFor="address" className="font-semibold text-lg">
+            Address:
+          </label>{" "}
+          <br />
+          <textarea
+            id="address"
+            name="address"
+            className="textarea textarea-info textarea-bordered textarea-xs w-full resize-none max-w-lg"
+          ></textarea>
+        </div>
+        <div className="mb-5">
+          <button className="btn bg-red-600 text-white text-xl w-full max-w-lg">
+            50% Discount (Sign Up Fee)
+          </button>
+        </div>
+        <div className="mb-5">
+          <label htmlFor="package" className="font-semibold text-lg">
+            Your Favorite Package:
+          </label>{" "}
+          <br />
+          <select
+            name="package"
+            id="package"
+            className="select select-info w-full max-w-lg"
+          >
+            <option value="">Select Package</option>
+            <option value="100">P1-100</option>
+            <option value="200">P2-200</option>
+            <option value="300">P3-300</option>
+          </select>
+        </div>
+        <div className="mb-5">
+          <label htmlFor="customer" className="font-semibold text-lg">
+            Type of Customer:
+          </label>{" "}
+          <br />
+          <input type="checkbox" name="PPPOE" id="" />
+          PPPOE
+          <input type="checkbox" name="Static" id="" />
+          Static
+          <input type="checkbox" name="Hotspot" id="" />
+          Hotspot
+        </div>
+        <div className="border max-w-lg p-5 mb-5">
+          <div className="mb-5">
+            <label htmlFor="referenceName" className="font-semibold text-lg">
+              Reference Name:
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              name="referenceName"
+              id="referenceName"
+              placeholder=""
+              className="input input-bordered input-info w-full max-w-lg"
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="referenceMobile" className="font-semibold text-lg">
+              Reference Mobile:
+            </label>{" "}
+            <br />
+            <input
+              type="tel"
+              name="referenceMobile"
+              id="referenceMobile"
+              placeholder=""
+              className="input input-bordered input-info w-full max-w-lg"
+            />
+          </div>
+        </div>
+        <input type="submit" name="" id="" value='Register' className="btn btn-info hover:bg-sky-600 w-[200px] text-white"/>
+        <button className="btn bg-gray-400 hover:bg-gray-600 w-[200px] text-white ml-3">Clear</button>
       </form>
     </div>
   );
